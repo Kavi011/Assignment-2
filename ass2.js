16a28c14234153a23beea4c271dabd9420e239fa
Kavi011 <88363985+Kavi011@users.noreply.github.com>
2021-08-04 21:03:43 +0530

    submitted

---
+++ ass2.js
@@ -0,0 +1,14 @@
+// Question 1
+
+let names = ["Kavi", "Subhi", "Raksh", "Akash", "Keer"]
+console.log(names.reverse())
+
+// question 2
+
+let i = 1;
+while (i <= 50) {
+if (i % 5 == 0) {
+console.log(i);
+}
+i++;
+}
